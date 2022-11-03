027710a71ed1be42bf1dad10a2dfabf4cfa2ac7f
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2022-11-04 00:26:38 +0100

    Commented map.h functions and definitions

--- include/map.h
+++ include/map.h
@@ -5,11 +5,35 @@
 #include "defs.h"
 #include "graphics.h"
 
-#define COLS 20
-#define ROWS 13
+#define COLS 20 ///< Number of columns used in the map.
+#define ROWS 13 ///< Number of rows used in the map.
 
-int get_map_tile_at(float, float);
+
+/**
+ * Returns the tile of the map at the given coordinates.
+ * @param[in] x The x coordinate belonging to a tile in the map.
+ * @param[in] y The y coordinate belonging to a tile in the map.
+ * @return The number of the tile, indicating the texture to use to draw it.
+ *
+ */
+int get_map_tile_at(float x, float y);
+
+
+/**
+ * Checks if the given coordinates are within the bounds of the map.
+ * @param[in] x The x coordinate to check.
+ * @param[in] y The y coordinate to check.
+ *
+ */
+bool is_inside_map(float x, float y);
+
+
+/**
+ * Draws the minimap on the screen.
+ * It uses the defined constant variable MINIMAP_SCALE_FACTOR to calculate the
+ * size of the minimap.
+ *
+ */
 void render_map(void);
-bool is_inside_map(float, float);
 
 #endif
